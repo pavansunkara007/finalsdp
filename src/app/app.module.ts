56710a8afa8b3bd57c0efedbc538b329d0687e80
pavansunkara007 <pavansunkara4767@gmail.com>
2020-12-11 22:36:09 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MaterialModule} from './material/material.module';
+import { MaterialModule} from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -21,20 +21,30 @@ export class BookingComponent implements OnInit {
     {name:"Dr.Ravi", sp:"General Surgeon", qua:"MBBS,MD", hosp:"Sunshine Hospital, Vijayawada ", exp:"15years"},
     {name:"Dr. Madhu ", sp:"Gynacologist", qua:"MBBS, DGO",hosp:"Rainbow Hospital, Visakapatanam", exp:"23years"},
     {name:"Dr.M.Latha", sp:"Dentist", qua:"BDS, MDS",hosp:"Clove Hospital, Bangalore", exp:"12years"},
-    {name:"Dr.S.Laya", sp:"Opthomologist", qua:"MBBS,MS Opthal",hosp:"Sono Vision Hospital, Chennai", exp:"17years"},
     {name:"Dr.V Srinivas", sp:"Neurologist", qua:"MBBS,MD, DM Neuro",hosp:"NIMS Hospital, Hyderabad", exp:"22years"},
     {name:"Dr. Saraswathi", sp:"ENT", qua:"MBBS, MS ENT",hosp:"Care Hospital, Bhubaneswar", exp:"13years"},
     {name:"Dr. Suresh", sp:"Veternary", qua:"MBBS,DVM",hosp:"Samaritam Veternary Care, Delhi", exp:"21years"},
     {name:"Dr.Maya", sp:"Dermatologist", qua:"MBBS,MD Derma",hosp:"Yashoda Hospital, Delhi", exp:"18years"},
     {name:"Dr.Kushi", sp:"Physchiaiatrist", qua:"MS Psychology",hosp:"Appollo Hospitals, Kolkata", exp:"10years"},
     {name:"Dr.Arya ", sp:"Gynacologist", qua:"MBBS, DGO",hosp:"Rainbow Hospital, Visakapatanam", exp:"23years"},
+    {name:"Dr.K Vijay", sp:"Pediatrician", qua:"MBBS",hosp:"NIMS Hospital, Hyderabad", exp:"12years"},
+    
+  ]
+  doctors1=[
+    {name:"Dr. Suresh", sp:"Veternary", qua:"MBBS,DVM",hosp:"Samaritam Veternary Care, Delhi", exp:"21years"},
+    {name:"Dr.Maya", sp:"Dermatologist", qua:"MBBS,MD Derma",hosp:"Yashoda Hospital, Delhi", exp:"18years"},
+    {name:"Dr.Kushi", sp:"Physchiaiatrist", qua:"MS Psychology",hosp:"Appollo Hospitals, Kolkata", exp:"10years"},
+    {name:"Dr.Arya ", sp:"Gynacologist", qua:"MBBS, DGO",hosp:"Rainbow Hospital, Visakapatanam", exp:"23years"},
     {name:"Dr.Hari", sp:"Dentist", qua:"BDS, MDS",hosp:"Clove Hospital, Bangalore", exp:"12years"},
-    ]
-
-
-  
-
+    {name:"Dr.S.Laya", sp:"Opthomologist", qua:"MBBS,MS Opthal",hosp:"Sono Vision Hospital, Chennai", exp:"17years"},
+    {name:"Dr.V Srinivas", sp:"Neurologist", qua:"MBBS,MD, DM Neuro",hosp:"NIMS Hospital, Hyderabad", exp:"22years"},
+    {name:"Dr.K SaiReddy", sp:"Cardioligist", qua:"MBBS,MD, DM Neuro",hosp:"KIMS Hospital, Hyderabad", exp:"12years"},
+    {name:"Dr.G Vamsi Mitra", sp:"Physio Therapist", qua:"MBBS,MD, DM Neuro",hosp:" Sree Physiocare, Hyderabad", exp:"20years"},
+    {name:"Dr.Hari", sp:"Dentist", qua:"BDS, MDS",hosp:"Clove Hospital, Bangalore", exp:"12years"},
+  ]
 }
+
+
 
 @Component({
   selector:'app-dialog',
