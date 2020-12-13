d2282bc6b77ef744033de08d59d7e759eb706d59
pavansunkara007 <pavansunkara4767@gmail.com>
2020-12-13 12:03:58 +0530

    Mailer Module added

--- database/mail.js
+++ database/mail.js
@@ -4,15 +4,15 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'pavansaisunkara@gmail.com',
-    pass: 'Pavan@15474767'
+    pass: 'Pavan@29284767'
   }
 });
 
 var mailOptions = {
   from: 'pavansaisunkara@gmail.com',
-  to: '',
-  subject: '2nd E-mail panichesthundi roy',
-  text: 'That was easy!'
+  to: 'ravi@gmail.com',
+  subject: 'Notification Alert',
+  text: 'Mr.Pavan wants to book an appointment with you on 13/12/20.'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
